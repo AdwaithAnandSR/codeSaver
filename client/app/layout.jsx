a2196be9d8cba209846a05a1da5ce4b86c683316
Adwaith Anand SR <adwaithanand1818@gmail.com>
2024-12-07 08:05:32 +0530

    Update layout.jsx

--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
 
          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-         <meta name="google-site-verification" content="your-verification-code" />
+         <meta name="google-site-verification" content="61qPDQWxI2NC98u55cV3fOXJJBTHun3aYZQyc83lPbA" />
 
          <body
             className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
